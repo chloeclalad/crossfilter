b27b1632df00ea4be6a370841bf214a5ce983b10
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2012-08-20 08:31:53 -0400

    Add coreops library to add more reducers

---
+++ coreops.js
@@ -0,0 +1,139 @@
+(function(exports){
+  _ = require("underscore");
+  coreops = {};
+  coreops.version = "0.0.1";
+
+  function coreops_reduceAdd(f) {
+    return function(p, v) {
+      return p + +f(v);
+    };
+  }
+
+  function coreops_reduceSubtract(f) {
+    return function(p, v) {
+      return p - f(v);
+    };
+  }
+
+
+  // Average
+  // Calculate the count and sum and return the average
+  // in value.value()
+  function coreops_reduceAddAverage(f) {
+    return function(p, v) {
+      p.count += 1;
+      p.sum += +f(v);
+      return p;
+    };
+  }
+
+  function coreops_reduceRemoveAverage(f) {
+    return function(p, v) {
+      p.count -= 1;
+      p.sum -= f(v);
+      return p;
+    };
+  }
+
+
+  // Extents
+  // Calculate the minimum and maximum values and return
+  // an array of [min, max] in value.value()
+  function coreops_reduceAddExtents(f) {
+    return function(p, v) {
+      p.candidates.push(f(v));
+      return p;
+    };
+  }
+
+  function coreops_reduceRemoveExtents(f) {
+    return function(p, v) {
+      var index = p.candidates.indexOf(f(v));
+      if (index >= 0)
+        p.candidates.splice(index, 1);
+      return p;
+    };
+  }
+
+  function coreops_initialExtents() {
+    return {
+      candidates: [],
+      value: function() {
+        var min, max;
+        _.each(this.candidates, function(d) {
+          if (min === undefined) min = d;
+          if (max === undefined) max = d;
+          if (d !== undefined && d < min) min = d;
+          if (d !== undefined && d > max) max = d;
+        })
+        return [min, max];
+      }
+    };
+  };
+
+  function coreops_reduceIncrement(p) {
+    return p + 1;
+  }
+
+  function coreops_reduceDecrement(p) {
+    return p - 1;
+  }
+
+
+  function coreops_initialZero() {
+    return 0;
+  }
+
+  // Calculate the total value in value
+  function sum(prop) {
+    var accessor = function(d) { return d[prop] };
+    return {
+            add: coreops_reduceAdd(accessor),
+            remove: coreops_reduceSubtract(accessor),
+            initial: coreops_initialZero
+          }
+  }
+
+  // Calculate the average value in value.value()
+  function average(prop) {
+    var accessor = function(d) { return d[prop] };
+    return {
+            add: coreops_reduceAddAverage(accessor),
+            remove: coreops_reduceSubtractAverage(accessor),
+            initial: function() { return {
+                count: 0,
+                sum: 0,
+                value: function() {
+                  return (this.count > 0) ? (this.sum / this.count) : 0;
+                }
+              };
+            }
+          }
+  }
+
+  // Calculate the [min, max] in value.value()
+  function extents(prop) {
+    var accessor = function(d) { return d[prop] };
+    return {
+            add: coreops_reduceAddExtents(accessor),
+            remove: coreops_reduceRemoveExtents(accessor),
+            initial: coreops_initialExtents
+    }
+  }
+
+  // Calculate the number of rows
+  // prop is not used, but included for consistency with other coreops calls
+  function count(prop) {
+    return {
+            add: coreops_reduceIncrement,
+            remove: coreops_reduceDecrement,
+            initial: coreops_initialZero
+          }
+  }
+
+  coreops.sum = sum;
+  coreops.count = count;
+  coreops.average = average;
+  coreops.extents = extents;
+  exports.coreops = coreops;
+})(this);
---
+++ test/coreops-test.js
@@ -0,0 +1,93 @@
+var vows = require("vows"),
+    assert = require("assert"),
+    d3 = require("d3"),
+    _ = require("underscore"),
+    crossfilter = require("../"),
+    coreops = require("../coreops.js");
+
+var suite = vows.describe("coreops");
+coreops = coreops.coreops;
+
+suite.addBatch({
+  "coreops": {
+    topic: function() {
+      var data = crossfilter([
+        {date: "2011-11-14T16:17:54Z", quantity: 2, total: 190, tip: 100, type: "tab"},
+        {date: "2011-11-14T16:20:19Z", quantity: 2, total: 190, tip: 100, type: "tab"},
+        {date: "2011-11-14T16:28:54Z", quantity: 1, total: 300, tip: 200, type: "visa"},
+        {date: "2011-11-14T16:30:43Z", quantity: 2, total: 90, tip: 0, type: "cash"},
+        {date: "2011-11-14T16:48:46Z", quantity: 2, total: 90, tip: 0, type: "tab"}
+      ]);
+
+      // be sure you don't clobber a built-in method if you do this!
+      try {
+        data.date = data.dimension(function(d) { return new Date(d.date); });
+        data.quantity = data.dimension(function(d) { return d.quantity; });
+        data.tip = data.dimension(function(d) { return d.tip; });
+        data.total = data.dimension(function(d) { return d.total; });
+        data.type = data.dimension(function(d) { return d.type; });
+      } catch (e) {
+        console.log(e.stack);
+      }
+
+      return data;
+    },
+
+    "version is correct": function(data) {
+      assert(coreops.version == '0.0.1');
+    },
+
+    "test sum": function(data) {
+      assert(data.tip.groupAll().reduce(coreops.sum('tip')).value() == 400);
+
+      // Testing group with reduceFactory
+      assert.deepEqual(data.type.group().reduce(coreops.sum('tip')).all(),
+          [ { key: 'cash', value: 0 },
+            { key: 'tab', value: 200 },
+            { key: 'visa', value: 200 } ]);
+
+    },
+
+    "test count": function(data) {
+      assert(data.tip.groupAll().reduce(coreops.count('tip')).value() == 5);
+      assert(data.tip.groupAll().reduce(coreops.count()).value() == 5);
+      // Testing group with reduceFactory
+      assert.deepEqual(data.type.group().reduce(coreops.count()).all(),
+          [ { key: 'cash', value: 1 },
+            { key: 'tab', value: 3 },
+            { key: 'visa', value: 1 } ]);
+    },
+
+    "test average": function(data) {
+      var val = data.tip.groupAll().reduce(coreops.average('tip')).value();
+      assert(val.value() == 80);
+      var grpval = data.type.group().reduce(coreops.average('tip')).all();
+      grpval = _.map(grpval, function(d) { return { key:d.key, value: d.value.value()}});
+      assert.deepEqual(grpval,
+          [ { value: 0, key: 'cash' },
+            { value: 66.66666666666667, key: 'tab' },
+            { value: 200, key: 'visa' }   ]);
+    },
+
+    "test extents": function(data) {
+      var val = data.tip.groupAll().reduce(coreops.extents('tip')).value();
+      assert.deepEqual(val.value(), [0, 200]);
+
+      var grpval = data.type.group().reduce(coreops.extents('tip')).all();
+      grpval = _.map(grpval, function(d) { return { key:d.key, value: d.value.value()}});
+      assert.deepEqual(grpval,
+          [ { value: [0, 0], key: 'cash' },
+            { value: [0, 100], key: 'tab' },
+            { value: [200, 200], key: 'visa' }   ]);
+
+    }
+
+
+  }
+});
+
+function key(d) {
+  return d.key;
+}
+
+suite.export(module);
